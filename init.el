4a2c0ffc28c120f123ff0bf7182e3c451b54b591
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2012-12-20 10:28:07 +0000

    Shorten eshell prompt

--- init.el
+++ init.el
@@ -77,15 +77,11 @@
 
 
 ;; JAVASCRIPT
-
-;; (autoload 'js-mode "js" nil t)
-;; (add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
 (require 'flymake-jshint)
-(add-hook 'js2-mode-hook
-     (lambda ()
-       (flymake-mode t)
-       (slime-js-minor-mode 1)))
-
+(add-hook 'js-mode-hook
+          (lambda ()
+            (flymake-mode t)))
 
 ;; CSS
 
@@ -124,18 +120,31 @@
 (setq yaml-indent-offset 2)
 
 
-<<<<<<< HEAD
 ;; SLIME
 (setq inferior-lisp-program "/usr/local/bin/clisp") ; your Lisp system
 (require 'slime)
 (slime-setup)
-=======
+
 ;; CLOJURE
 
 (require 'clojure-mode)
 
 
->>>>>>> FETCH_HEAD
+;; SQL
+
+
+(add-hook 'sql-mode-hook
+          (lambda ()
+            (sql-highlight-mysql-keywords)))
+
+
+;; ESHELL
+(setq eshell-prompt-function
+      (lambda nil
+        (concat (car
+                 (last
+                  (split-string (eshell/pwd) "/")))
+                " ")))
 
 
 ;; *********************
@@ -153,15 +162,10 @@
  '(org-agenda-files (quote ("~/Dropbox/org/journal.org" "~/Dropbox/org/general.org")))
  '(org-alphabetical-lists t)
  '(org-clock-into-drawer 2)
-<<<<<<< HEAD
  '(org-global-properties (quote (("Effort_ALL" . "0 0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))))
  '(python-shell-interpreter "ipython")
  '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/Users/twraight/Envs/dashboard/bin/python") (pony-settings make-pony-project :python "/Users/twraight/Envs/dashboard/bin/python" :settings "www/conf/local.py"))))
  '(w3m-use-cookies t)
-=======
- '(org-global-properties (quote (("Effort_ALL" . "0 0:05 0:10 0:20 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))))
- '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "~/Envs/grace/bin/python" :settings "settings"))))
->>>>>>> FETCH_HEAD
  '(yaml-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -186,7 +190,7 @@
  '(org-meta-line ((t (:inherit org-block-begin-line))))
  '(org-special-keyword ((t (:inherit font-lock-keyword-face :foreground "#003441"))))
  '(org-table ((t (:foreground "LightSkyBlue" :height 1 :family "Menlo"))))
- '(variable-pitch ((t (:height 1.4 :family "Lucida Grande"))))
+ '(variable-pitch ((t (:height 1.3 :family "Lucida Grande"))))
  '(w3m-session-select ((t (:foreground "white" :family "Gill Sans")))))
 
 
