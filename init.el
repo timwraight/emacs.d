0ff5f5c0ac05e93465a0f481a722e42afdf6db47
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2012-12-31 10:35:20 +0000

    Make eshell regex compatbile with new prompt

--- init.el
+++ init.el
@@ -151,6 +151,8 @@
                   (split-string (eshell/pwd) "/")))
                 " ")))
 
+(setq eshell-prompt-regexp "^[^#$\n]* ")
+
 
 ;; *********************
 ;; EMACS-GENERATED STUFF
@@ -168,8 +170,8 @@
  '(org-alphabetical-lists t)
  '(org-clock-into-drawer 2)
  '(org-global-properties (quote (("Effort_ALL" . "0 0:05 0:10 0:20 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))))
- '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "~/Envs/grace/bin/python" :settings "settings"))))
  '(python-shell-interpreter "ipython")
+ '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/Users/twraight/Envs/dashboard/bin/python") (pony-settings make-pony-project :python "~/Envs/grace/bin/python" :settings "settings"))))
  '(yaml-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
