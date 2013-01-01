a54ef5fcb6380c29de5660d2cef577f3fd4743ac
Tim Wraight <tim@wraight.net>
2013-01-01 17:27:07 +0000

    Add fix for pony-mode

--- python.el
+++ python.el
@@ -47,4 +47,9 @@
             ; (eldoc-mode 1)
 	    (add-to-list 'ac-sources 'ac-source-ropemacs)))
 
+(remove-hook 'python-mode-hook 'wisent-python-default-setup)
 
+;; NOSETESTS
+;; Don't need this right now, as I should just be able to do it from Pony-mode
+;; (require 'nose)
+;; (defvar nose-use-verbose nil) 
