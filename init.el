cc7512b7ed0ec5a3fc8a9a4b46ec0e6358603071
Tim Wraight <tim@wraight.net>
2013-01-13 21:04:03 +0000

    Add scheme support

--- init.el
+++ init.el
@@ -124,6 +124,11 @@
 
 ;; SCHEME
 (load-file "~/.emacs.d/vendor/geiser/elisp/geiser.el")
+(setq geiser-active-implementations '(guile))
+(add-hook 'scheme-mode-hook
+          (lambda ()
+            (geiser-mode)
+            (run-guile)))
 
 ;; CLOJURE
 
@@ -187,6 +192,7 @@
  '(org-global-properties (quote (("Effort_ALL" . "0 0:05 0:10 0:20 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00"))))
  '(python-shell-interpreter "ipython")
  '(safe-local-variable-values (quote ((pony-settings make-pony-project :python "/Users/twraight/Envs/dashboard/bin/python") (pony-settings make-pony-project :python "~/Envs/grace/bin/python" :settings "settings"))))
+ '(scheme-program-name "guile")
  '(yaml-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
