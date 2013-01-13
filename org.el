0d5d50ad5d6b40d274e82083d65e05acd1908b49
Tim Wraight <tim@wraight.net>
2013-01-13 22:02:18 +0000

    Change TODO to action.
    Means I can mark a headline as needing action, without needing to make
    the headline text itself a verb.

--- org.el
+++ org.el
@@ -7,7 +7,7 @@
 (setq org-default-notes-file "~/Dropbox/org/general.org")
 
 ; Keywords
-(setq org-todo-keywords '((sequence "TODO" "NEXT" "|" "DONE")))
+(setq org-todo-keywords '((sequence "ACTION" "DONE")))
 
 ; Clocking
 (global-set-key (kbd "S-<f13>") 'org-clock-out)
