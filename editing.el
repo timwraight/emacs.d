daae5a4a42464d43d77d0ca04078068420c4069a
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-01-22 16:37:14 +0000

    Ignore style errors in git-commit mode

--- editing.el
+++ editing.el
@@ -157,6 +157,7 @@
 (add-hook 'git-commit-mode-hook 'turn-on-flyspell)
 (add-hook 'git-commit-mode-hook (lambda () (setq save-place 0)))
 (setq magit-save-some-buffers 'dontask)
+(setq git-commit-ignore-style-errors t)
 
 
 ;; VIMP
