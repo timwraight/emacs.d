301fb0b50a47f326c0f1b014f5213d8051150919
Tim Wraight <tim@wraight.net>
2013-01-30 20:14:46 +0000

    Fix syntax error

--- editing.el
+++ editing.el
@@ -172,7 +172,7 @@
 (require 'vimp )
 (vimp-mode 1)
 (global-set-key (kbd "<f1>") 'vimp-local-mode)
-(global-set-key (kbd "C-|") (lambda () (vimp-motion-state t))
+(global-set-key (kbd "C-|") (lambda () (vimp-motion-state t)))
 
 ; Use visual lines with j and k
 (define-key vimp-motion-state-map "j" 'vimp-next-visual-line)
