a03def10cf7360e2125a177621bad9ddd713268d
Tim Wraight <tim@wraight.net>
2013-02-05 13:48:28 +0000

    Sort email by most recent

--- gnus.el
+++ gnus.el
@@ -2,5 +2,9 @@
 (require 'gnus-load)
 (add-hook 'gnus-article-mode-hook (lambda () (variable-pitch-mode t)))
 
+(setq gnus-thread-sort-functions
+           '(gnus-thread-sort-by-number
+             gnus-thread-sort-by-most-recent-date))
+
 (shell-command "offlineimap&" "*offlineimap*" nil)
 
