b235b25647830856fdc49a93b2ed2aa15d0e5c7e
Tim Wraight <tim@wraight.net>
2013-02-14 13:54:23 +0000

    Set gnus asynchronous mode

--- gnus.el
+++ gnus.el
@@ -1,6 +1,7 @@
 ; Also check .gnus. 
 (require 'gnus-load)
 (add-hook 'gnus-article-mode-hook (lambda () (variable-pitch-mode t)))
+(setq gnus-asynchronous t)
 
 (setq gnus-thread-sort-functions
            '(gnus-thread-sort-by-number
