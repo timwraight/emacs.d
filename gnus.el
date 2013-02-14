977ae8876962f1af486ef39c70c679dd041bb773
Tim Wraight <tim@wraight.net>
2013-02-14 13:55:25 +0000

    Add google contacts for gnus

--- gnus.el
+++ gnus.el
@@ -6,4 +6,9 @@
 (setq gnus-thread-sort-functions
            '(gnus-thread-sort-by-number
              gnus-thread-sort-by-most-recent-date))
+
+(require 'google-contacts)
+(require 'google-contacts-gnus)
+(require 'google-contacts-message)
+
 (gnus-demon-add-handler 'gnus-demon-scan-news 2 t) 
