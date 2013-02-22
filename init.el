34e4cfc941795b6aec4749ed19eb6fab8ed1e02e
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-02-22 14:44:01 +0000

    Change default font size
    We should find a way to get this in the setup-specific file

--- init.el
+++ init.el
@@ -46,7 +46,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#999999" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "apple" :family "Consolas"))))
+ '(default ((t (:inherit nil :stipple nil :background "#042028" :foreground "#999999" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "Microsoft" :family "Consolas"))))
  '(cursor ((t (:background "#fffece" :foreground "#042028"))))
  '(fixed-pitch ((t (:height 140 :family "Consolas"))))
  '(link ((t (:foreground "#9d8464" :inverse-video nil :underline t :slant normal :weight normal))))
