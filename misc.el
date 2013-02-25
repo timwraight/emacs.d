77bec39337cb737cda932f89ecdcc4fb02b012f2
Tim Wraight <tim@wraight.net>
2013-02-25 10:55:13 +0000

    Add SSH config mode

--- misc.el
+++ misc.el
@@ -192,6 +192,13 @@
 (autoload 'pcomplete/ack-grep "pcmpl-ack")
 
 
+;; SSH CONFIG MODE
+(autoload 'ssh-config-mode "ssh-config-mode" t)
+(add-to-list 'auto-mode-alist '(".ssh/config\\'"  . ssh-config-mode))
+(add-to-list 'auto-mode-alist '("sshd?_config\\'" . ssh-config-mode))
+(add-hook 'ssh-config-mode-hook 'turn-on-font-lock)
+
+
 ;; SUPER-MISC!!
 
 ; This should really go in navigation.el, but seems to be overridden
