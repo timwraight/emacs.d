72e08a3f21ab125f0a22d5144e9a83a78a5a5e97
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-03-04 09:35:36 +0000

    Remove random thing of unknown purpose

--- init.el
+++ init.el
@@ -30,7 +30,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-indent-next-pair-timer-geo-mean (quote ((default 0.0005 0))))
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
  '(debug-on-error nil)
  '(org-alphabetical-lists t)
