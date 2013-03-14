7f8ea4181169f0d5ebe9d75a8d2f80a736ce4a68
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-03-14 10:23:04 +0000

    Revert "Switch to python.el with ipython"
    This reverts commit ecbf3cc2482b15fe4bb229a39a7c4608f3055ccb.

--- python.el
+++ python.el
@@ -1,14 +1,4 @@
-(setq
- python-shell-interpreter "ipython"
- python-shell-interpreter-args ""
- python-shell-prompt-regexp "In \\[[0-9]+\\]: "
- python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
- python-shell-completion-setup-code
-   "from IPython.core.completerlib import module_completion"
- python-shell-completion-module-string-code
-   "';'.join(module_completion('''%s'''))\n"
- python-shell-completion-string-code
-   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+(setq py-shell-name "ipython")
 
 ;; HOOKS
 
@@ -21,11 +11,44 @@
         ))
 
 
-;; JEDI
-(el-get-install "jedi")
-(require 'jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
-(add-hook 'ein:connect-mode-hook 'ein:jedi-setup)
+;; DOCS
+
+(eval-when-compile (require 'pylookup))
+(setq pylookup-program "~/.emacs.d/vendor/pylookup/pylookup.py")
+(setq pylookup-db-file "~/.emacs.d/vendor/pylookup/pylookup.db")
+(autoload 'pylookup-update "pylookup" 
+  "Run pylookup-update and create the database at `pylookup-db-file'." t)
+(global-set-key "\C-ch" 'pylookup-lookup)
+
+
+;; PYMACS
+(require 'pymacs)
+;; avoid problems with pymacs (see http://pymacs.progiciels-bpi.ca/index.html)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+(setq py-load-pymacs-p nil)
+
+;; ROPEMACS
+
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-guess-project t)
+(setq ropemacs-separate-doc-buffer t)
+(setq ropemacs-enable-autoimport nil)
+
 
 ;; DJANGO
-(require 'python-django)
+
+(require 'pony-mode)
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (pony-key (kbd "C-c C-p C-s") 'pony-south-schemamigration)
+            (pony-key (kbd "C-c C-p C-a") 'pony-south-migrate)))
+
+(remove-hook 'python-mode-hook 'wisent-python-default-setup)
+(setq pony-test-failfast t)
+
