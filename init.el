f37fd0252f562ba5dda531201411e8f2d2294b2d
Tim Wraight <tim@wraight.net>
2013-03-14 07:35:57 +0000

    Add el-get

--- init.el
+++ init.el
@@ -15,6 +15,22 @@
   (load-file (expand-file-name file user-init-dir)))
 
 
+;; EL-GET
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
+
+
+
 (load-user-file "packages.el")
 (load-user-file "interface.el")
 (load-user-file "python.el")
