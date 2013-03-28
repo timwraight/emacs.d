34954b674272e0db364a6e2213a769cf99da8ddd
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-03-28 09:05:22 +0000

    Use JS2 mode and JSS

--- misc.el
+++ misc.el
@@ -68,11 +68,15 @@
 (add-hook 'nxml-mode-hook (lambda () (variable-pitch-mode nil)))
 
 ;; JAVASCRIPT
-(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
-(add-hook 'js-mode-hook
-          (lambda ()
-            (interactive)
-            (setq truncate-lines 0)))
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(eval-after-load "js2-mode"
+  '(progn
+     (setq js2-mirror-mode nil)))
+
+(require 'jss)
+
 
 ;; CSS
 
