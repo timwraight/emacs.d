371b4afb1870b8c5b84573c67447705035627960
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-04-15 10:24:51 +0100

    Add multiple cursors

--- editing.el
+++ editing.el
@@ -237,3 +237,10 @@
 ;; PAREDIT
 ; Minor mode for editing parentheses
 (require 'paredit)
+
+;; Multiple cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
