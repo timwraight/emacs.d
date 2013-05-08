5786b05181c4720ad3ec22a75cf9a6b88caa23f3
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-05-08 17:35:42 +0100

    Correct regexps for markdown files

--- misc.el
+++ misc.el
@@ -98,9 +98,11 @@
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\$" . markdown-mode))
-(add-hook 'markdown-mode-hook (lambda () (interactive) (variable-pitch-mode nil)))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-hook 'markdown-mode-hook (lambda () (interactive)
+                                (variable-pitch-mode nil)
+                                (auto-fill-mode)))
 
 
 
