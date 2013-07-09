e3ee4c408c7d5f55ca7b4da299393b211bf28319
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-07-09 15:38:06 +0100

    Don't put backup files in the working tree

--- editing.el
+++ editing.el
@@ -37,6 +37,19 @@
 (add-hook 'latex-mode-hook 'turn-off-auto-fill)
 
 
+;; BACKUPS
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
+
+
 ;; SPELLING
 (setq ispell-program-name "aspell"
       ispell-dictionary "british"
