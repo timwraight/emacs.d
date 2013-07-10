e4dacd5a4ff7b8fa979430812e0899d1797d0dd8
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-07-10 09:18:18 +0100

    Move some setup-specific stuff out of init-mu4e file

--- custom/el-get/setup/init-mu4e.el
+++ custom/el-get/setup/init-mu4e.el
@@ -1,10 +1,5 @@
 ;; general emacs mail settings; used when composing e-mail
 ;; the non-mu4e-* stuff is inherited from emacs/message-mode
-(setq mu4e-reply-to-address "Tim.Wraight@tangentlabs.co.uk"
-      mu4e-user-mail-address-list '("Tim.Wraight@tangentlabs.co.uk")
-      user-mail-address "Tim.Wraight@tangentlabs.co.uk"
-      user-full-name  "Tim Wraight")
-
 ;; MU4E, MU for Emacs, a mail client
 
 ;; Vimp keybindings for mu4e
@@ -30,15 +25,15 @@
      ;; "q" 'mu4e-raw-view-quit-buffer)
      
      (vimp-add-hjkl-bindings mu4e-headers-mode-map 'normal
-                             "J" 'mu4e~headers-jump-to-maildir
-                            "j" 'vimp-next-line
-                            "C" 'mu4e-compose-new
-                            "o" 'mu4e-view-message)
+       "J" 'mu4e~headers-jump-to-maildir
+       "j" 'vimp-next-line
+       "C" 'mu4e-compose-new
+       "o" 'mu4e-view-message)
      
      (vimp-add-hjkl-bindings mu4e-main-mode-map 'normal
-                             "J" 'mu4e~headers-jump-to-maildir
-                             "j" 'vimp-next-line
-                            "RET" 'mu4e-view-message)))
+       "J" 'mu4e~headers-jump-to-maildir
+       "j" 'vimp-next-line
+       "RET" 'mu4e-view-message)))
 
 (setq mu4e-maildir "~/Mail")
 (add-hook 'mu4e-main-mode-hook 'vimp-insert-state)
@@ -60,21 +55,10 @@
  mu4e-headers-visible-lines 16
  mu4e-use-fancy-chars t)
 
-(setq mu4e-drafts-folder "/drafts")
-(setq mu4e-sent-folder   "/TangentTim/INBOX.Sent Messages")
-(setq mu4e-trash-folder  "/TangentTim/INBOX.Deleted Messages")
 
 (setq mu4e-compose-complete-only-personal t)
 (setq mu4e-html2text-command "html2text -utf8 -width 72")
 (add-to-list 'ac-modes 'mu4e-compose-mode)
-
- (setq mu4e-maildir-shortcuts
-       '( ("/TangentTim/INBOX" . ?i)
-          ("/TangentTim/INBOX.Sent Messages" . ?s)
-          ("/TangentTim/INBOX.Deleted Messages" . ?t)
-          ("/TangentTim/INBOX.Archive" . ?a)
-          ("/TangentTim/INBOX.Errors.Dashboard" . ?e)
-          ("/TangentTim/INBOX.Wolseley Data" . ?d)))
 
 (setq mu4e-attachment-dir  "~/Downloads")
 
@@ -99,15 +83,8 @@
 (setq gnus-dired-mail-mode 'mu4e-user-agent)
 (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
 
+(setq mu4e-headers-leave-behavior 'apply)
 
-;; smtp mail setting
-(setq
-     message-send-mail-function 'smtpmail-send-it
-     smtpmail-smtp-server "mail.tangentlabs.co.uk"
-     ;; if you need offline mode, set these -- and create the queue dir
-     ;; with 'mu mkdir', i.e.. mu mkdir /home/user/Maildir/queue
-     smtpmail-queue-mail  nil
-     smtpmail-queue-dir  "/Users/twraight/Mail/queue/cur")
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
