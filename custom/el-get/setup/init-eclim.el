f4196f3800b70826ec93e923464f2bb5ac0160a8
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2013-11-28 14:59:44 +0000

    Set up eclim help to display automatically

--- custom/el-get/setup/init-eclim.el
+++ custom/el-get/setup/init-eclim.el
@@ -6,6 +6,11 @@
 (ac-config-default)
 (add-hook 'java-mode-hook 'eclim-mode)
 
+;; show errors when on top of them
+(setq help-at-pt-display-when-idle t)
+(setq help-at-pt-timer-delay 0.1)
+(help-at-pt-set-timer)
+
 ;; add the emacs-eclim source
 (require 'ac-emacs-eclim-source)
 (ac-emacs-eclim-config)
