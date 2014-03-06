217f2aebd95a523d8dc83c66e6f7785c2a5ab353
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-03-06 11:44:37 +0000

    Let flycheck do JS linting

--- custom/el-get/setup/init-js2-mode.el
+++ custom/el-get/setup/init-js2-mode.el
@@ -2,13 +2,10 @@
 
 (eval-after-load "js2-mode"
   '(progn
-     (setq js2-mirror-mode nil)))
-
-(require 'key-chord)
-(require 'helm)
-(key-chord-define js2-mode-map ",/" 'helm-javascript-functions)
-(define-key js2-mode-map (kbd "<M-RET>") 'auto-indent-eol-char-newline)
-;; Let flycheck handle parse errors
-(setq-default js2-show-parse-errors nil)
-(setq-default js2-strict-missing-semi-warning nil)
-(setq-default js2-strict-trailing-comma-warning nil)
+     (setq js2-mirror-mode nil)
+     (key-chord-define js2-mode-map ",/" 'helm-javascript-functions)
+     (define-key js2-mode-map (kbd "<M-RET>") 'auto-indent-eol-char-newline)
+     ;; Let flycheck handle parse errors
+     (setq-default js2-show-parse-errors nil)
+     (setq-default js2-strict-missing-semi-warning nil)
+     (setq-default js2-strict-trailing-comma-warning nil)))
