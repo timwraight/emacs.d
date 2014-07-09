556ccdbca51f9c7ea0e55f1e48089cf1a246bd11
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-07-09 12:28:43 +0100

    Jedi in other window by default

--- custom/el-get/setup/init-jedi.el
+++ custom/el-get/setup/init-jedi.el
@@ -2,3 +2,11 @@
 ;; (add-hook 'jedi-mode-hook 'jedi-direx:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:setup-keys t)
+(setq jedi:goto-definition-config '((t nil nil)        ; C-.
+                                    (nil nil nil)          ; C-u C-.
+                                    (nil definition nil)
+                                    (t definition nil)
+                                    (nil nil t)
+                                    (t nil t)
+                                    (nil definition t)
+                                    (t definition t)))
