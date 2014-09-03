fa9e545182c42f4ca646dac415a452fbe58099d9
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-09-03 13:10:04 +0100

    Add docs for go to definition

--- custom/el-get/setup/init-jedi.el
+++ custom/el-get/setup/init-jedi.el
@@ -2,9 +2,12 @@
 ;; (add-hook 'jedi-mode-hook 'jedi-direx:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:setup-keys t)
-(setq jedi:goto-definition-config '((t nil nil)        ; C-.
+; Note that the second param here controls whether jedi jumps to the definition
+; of the function or its assignment. nil means go to assignment. And the first
+; arg is whether to open in new window or not.
+(setq jedi:goto-definition-config '((t definition nil)     ; C-.
                                     (nil nil nil)          ; C-u C-.
-                                    (nil definition nil)
+                                    (t nil nil)            ; C-u C-u C-.
                                     (t definition nil)
                                     (nil nil t)
                                     (t nil t)
