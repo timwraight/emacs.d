7b80a0121fec4041df07da834081a011206b5f28
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-10-13 16:37:33 +0100

    Reconnect on lost jabber connection

--- custom/el-get/setup/init-emacs-jabber.el
+++ custom/el-get/setup/init-emacs-jabber.el
@@ -1,4 +1,21 @@
 (require 'jabber-autoloads)
 (setq jabber-default-connection-type 'network)
 (setq jabber-mode-line-presence)
+(setq jabber-auto-reconnect t)
 (setq jabber-alert-presence-message-function nil)
+
+(require 'alert)
+
+(defun notify-jabber-notify (from buf text proposed-alert)
+  "(jabber.el hook) Notify of new Jabber chat messages via notify.el"
+  (when (or jabber-message-alert-same-buffer
+            (not (memq (selected-window) (get-buffer-window-list buf))))
+    (if (jabber-muc-sender-p from)
+        (alert (format "(PM) %s"
+                       (jabber-jid-displayname (jabber-jid-user from)))
+               (format "%s: %s" (jabber-jid-resource from) text)))
+      (alert (format "%s" (jabber-jid-displayname from))
+             text)))
+
+(add-hook 'jabber-alert-message-hooks 'notify-jabber-notify)
+(add-hook 'jabber-chat-mode-hook 'vimp-insert-state)
