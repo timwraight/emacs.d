17ee017f1f02c2d4990b75735573afb89d3a0ee6
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-10-13 16:37:59 +0100

    Only delete trailing whitespace in python mode

--- python.el
+++ python.el
@@ -3,7 +3,12 @@
       (lambda ()
         (setq ac-sources '(ac-source-symbols ac-source-words-in-same-mode-buffers))))
 
-(add-hook 'python-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(add-hook 'python-mode-hook
+      '(lambda ()
+         (add-hook 'before-save-hook
+                   (lambda ()
+                     (delete-trailing-whitespace)))))
+
 (define-key python-mode-map (kbd "C-c d") 'jedi:show-doc)
 (setq python-indent-offset 4)
 
