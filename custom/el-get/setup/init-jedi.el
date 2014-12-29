91faf640cbce6395f93192574bca6381f66cae90
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2014-12-29 13:30:48 +0000

    Recenter jedi after going to definition

--- custom/el-get/setup/init-jedi.el
+++ custom/el-get/setup/init-jedi.el
@@ -2,6 +2,15 @@
 ;; (add-hook 'jedi-mode-hook 'jedi-direx:setup)
 (setq jedi:complete-on-dot t)
 (setq jedi:setup-keys t)
+(setq jedi:tooltip-method nil)
+
+(define-key python-mode-map (kbd "M-.")
+    (lambda ()
+        ((progn)
+         (jedi:goto-definition)
+         (recenter-top-bottom 0)
+         )))
+
 ; Note that the second param here controls whether jedi jumps to the definition
 ; of the function or its assignment. nil means go to assignment. And the first
 ; arg is whether to open in new window or not.
