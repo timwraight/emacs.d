9f44c0c997d2b27953a59fde1b918bdc0a35b826
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2015-10-06 13:39:37 +0100

    Add melpa

--- packages.el
+++ packages.el
@@ -1,3 +1,11 @@
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (unless (require 'el-get nil 'noerror)
