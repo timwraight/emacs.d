a234c0322ef49d76e81794cf656548ba32d469e8
Tim Wraight <tim.wraight@tangentlabs.co.uk>
2015-10-23 08:48:50 +0100

    Enable tagedit globally

--- tim_setup/init-tagedit.el
+++ tim_setup/init-tagedit.el
@@ -2,6 +2,6 @@
 (define-key html-mode-map (kbd "M-i") 'tagedit-forward-slurp-tag)
 (define-key html-mode-map (kbd "M-n") 'tagedit-forward-barf-tag)
 (tagedit-add-experimental-features)
-(add-hook 'sgml-mode-hook 'tagedit-mode)
+(add-hook 'sgml-mode-hook (lambda () (interactive) (tagedit-mode 1)))
 
 
--- tim_setup/misc.el
+++ tim_setup/misc.el
@@ -94,21 +94,6 @@
 (setenv "DYLD_LIBRARY_PATH" "/usr/local/mysql/lib:$DYLD_LIBRARY_PATH")
 
 
-;; Clipboard integration
-
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx) 
-
-
 ; ERC
 
 (add-hook 'erc-join-hook (lambda ()
