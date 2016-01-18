6325614b314b26066226a0742a197b245c2b7988
Tim Wraight <tim@wraight.net>
2016-01-18 20:58:10 +0000

    Set python-mode to use a monospaced font
    So that I can use the nicer looking default font elsewhere

--- tim_setup/init-python.el
+++ tim_setup/init-python.el
@@ -1,2 +1,13 @@
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "-i")
+; Make sure to actually define the switch to monospaced function, or
+;; the next bit won't work
+;; do it in setup-specific so that you can use a font you actually have.
+;; You can use code like this:
+;;
+;; (defun buffer-switch-to-monospaced ()
+;;   (interactive)
+;;   (face-remap-add-relative 'default :family "Consolas")
+;;   )
+
+(add-hook 'python-mode-hook 'buffer-switch-to-monospaced)
