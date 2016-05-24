116282b87836afc4b138f93e112da2c82fef152a
Tim Wraight <tim@wraight.net>
2016-05-24 15:58:38 +0100

    Add desktop mode

--- tim_setup/interface.el
+++ tim_setup/interface.el
@@ -69,3 +69,8 @@
 ;; EDIFF
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 (setq ediff-split-window-function 'split-window-horizontally)
+
+
+;; DESKTOP
+(setq desktop-path "~/.emacs.d/.emacs.desktop")
+(desktop-save-mode 1)
