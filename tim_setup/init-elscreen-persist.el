d332a749069c9237e1e5a24bc06af3cd2adfc646
Tim Wraight <tim@wraight.net>
2016-05-24 15:59:03 +0100

    Get rid of el-screen persist
    Seems to have been breaking things

--- tim_setup/init-elscreen-persist.el
+++
@@ -1,4 +0,0 @@
-(elscreen-start)
-(elscreen-persist-mode 1)
-(elscreen-persist-restore)
-(add-hook 'kill-emacs-hook 'elscreen-persist-store)
--- tim_setup/init-which-key.el
+++ tim_setup/init-which-key.el
@@ -1,1 +1,2 @@
 (which-key-mode)
+(setq which-key-sort-order 'which-key-prefix-then-key-order)
