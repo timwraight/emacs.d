b580d24b646e79e3c3a7f72cf3854cd441d97f94
Tim Wraight <tim.wraight@octoenergy.com>
2017-08-15 17:11:16 +0100

    Add some evil mc stuff

--- custom_packages/final-init.el
+++ custom_packages/final-init.el
@@ -18,7 +18,31 @@
 (define-key evil-normal-state-map (kbd "M-C-n") 'evil-first-non-blank)
 
 ;; (define-key evil-insert-state-map (kbd "M-z") (lambda () (interactive) (newline-and-indent) (previous-line) (indent-according-to-mode)))
-(define-key evil-insert-state-map (kbd "M-z") 'evil-open-above)
+(define-key evil-insert-state-map (kbd "M-z") 'open-line)
 (define-key evil-insert-state-map (kbd "M-j") 'evil-join)
 (define-key yas-minor-mode-map (kbd "M-t") 'yas-expand)
 (define-key evil-normal-state-map (kbd "o") 'switch-window)
+
+
+(define-key evil-normal-state-map (kbd "M-t")
+  (lambda ()
+    (interactive)
+    (message "Making cursor at point...")
+    (evil-mc-pause-cursors)
+    (evil-mc-make-cursor-here)
+    )
+  )
+(define-key evil-normal-state-map (kbd "M-r")
+  (lambda ()
+    (interactive)
+    (if (evil-mc-frozen-p)
+        (progn
+          (message "Resuming cursors...")
+          (evil-mc-resume-cursors)
+          )
+      (progn
+        (message "Pausing cursors...")
+        (evil-mc-pause-cursors)))))
+(define-key evil-normal-state-map (kbd "M-d") 'evil-mc-undo-cursor-at-pos)
+(add-hook 'evil-insert-state-entry-hook 'evil-mc-resume-cursors)
+(add-hook 'evil-normal-state-entry-hook 'evil-mc-pause-cursors)
--- tim_setup/init-evil-mc.el
+++ tim_setup/init-evil-mc.el
@@ -1,5 +1,7 @@
 (require 'evil-mc)
 (evil-define-key 'normal evil-mc-key-map (kbd "M-n") 'evil-backward-little-word-begin)
+(evil-define-key 'normal evil-mc-key-map (kbd "C-p") 'undo-tree-redo)
+
 (setq evil-mc-undo-cursors-on-keyboard-quit t)
 (global-evil-mc-mode 1)
 (add-hook 'evil-mc-mode-hook 'evil-mc-pause-cursors)
