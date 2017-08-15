d6218fef925a396e9bf8ce94bf95d98b8f6df125
Tim Wraight <tim.wraight@octoenergy.com>
2017-08-15 17:37:15 +0100

    Move company keybindings to final init

--- custom_packages/final-init.el
+++ custom_packages/final-init.el
@@ -46,3 +46,35 @@
 (define-key evil-normal-state-map (kbd "M-d") 'evil-mc-undo-cursor-at-pos)
 (add-hook 'evil-insert-state-entry-hook 'evil-mc-resume-cursors)
 (add-hook 'evil-normal-state-entry-hook 'evil-mc-pause-cursors)
+
+
+;; COMPANY BINDINGS
+
+;; Seems like I need to put *these* bindings even *laster*, at least if I want to run in a GUI,
+;; which I do want to now.
+(define-key company-active-map (kbd "M-i") 'company-complete-selection)
+(define-key company-active-map (kbd "M-s") 'company-filter-candidates)
+(define-key company-active-map (kbd "M-e") 'company-select-next)
+(define-key company-active-map (kbd "M-n") 'company-abort)
+(define-key company-active-map (kbd "M-u") 'company-select-previous)
+(define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "M-l") 'company-show-location)
+(define-key company-active-map (kbd "M-m") 'company-filter-candidates)
+
+(define-key company-active-map [?\M-i] 'company-complete-selection)
+(define-key company-active-map [?\M-s] 'company-filter-candidates)
+(define-key company-active-map [?\M-e] 'company-select-next)
+(define-key company-active-map [?\M-t] 'company-abort)
+(define-key company-active-map [?\M-u] 'company-select-previous)
+(define-key company-active-map [?\M-d] 'company-show-doc-buffer)
+(define-key company-active-map [?\M-l] 'company-show-location)
+
+(define-key company-filter-map (kbd "M-e") 'company-select-next)
+(define-key company-filter-map (kbd "M-u") 'company-select-previous)
+(define-key company-filter-map (kbd "M-i") 'company-complete-selection)
+(define-key company-search-map (kbd "<SPC>") 'company-abort)
+
+(define-key company-search-map (kbd "M-e") 'company-select-next)
+(define-key company-search-map (kbd "M-u") 'company-select-previous)
+(define-key company-search-map (kbd "M-i") 'company-complete-selection)
+(define-key company-search-map (kbd "<SPC>") 'company-abort)
