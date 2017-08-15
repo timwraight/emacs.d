768c1f27bef74e97c0417b8e40680f782e711703
Tim Wraight <tim.wraight@octoenergy.com>
2017-08-15 17:11:45 +0100

    Update ctags config

--- tim_setup/init-ctags-update.el
+++ tim_setup/init-ctags-update.el
@@ -1,2 +1,3 @@
-(autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on 'ctags-auto-update-mode'." t)
-(add-hook 'python-mode-hook  'turn-on-ctags-auto-update-mode)
+(require 'ctags-update)
+
+(add-hook 'python-mode-hook 'turn-on-ctags-auto-update-mode)
